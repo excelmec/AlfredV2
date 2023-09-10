c4976f87752bc8575d1190c423a543d3b46a1ff6
Joel K George <joelkgeorge@Joels-MBP.local>
2023-09-10 20:56:08 +0530

    Base finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-import SideNav from './common/SideNav/SideNav'
-import TopNav from './common/TopNav/TopNav'
 import CampusAmbassador from './pages/CampusAmbassador/CampusAmbassador'
 
 function App() {
