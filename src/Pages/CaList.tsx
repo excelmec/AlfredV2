0d03dc22fbb6f97156d63fac9bf91e5e2660625a
Mayon-Francis <mayonfrancis@gmail.com>
2023-10-23 21:58:04 +0530

    filter toolbar

--- src/Pages/CaList.tsx
+++ src/Pages/CaList.tsx
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { CA, getCaList } from '../utils/api';
 
@@ -42,17 +42,37 @@ export default function CaList() {
 					width: '90%',
 				}}
 				autoPageSize
-				pageSizeOptions={[5, 10, 25]}
+				slots={{ toolbar: GridToolbar }}
+				slotProps={{
+					toolbar: {
+						showQuickFilter: true,
+					},
+				}}
 			/>
 		</>
 	);
 }
 
 const columns: GridColDef[] = [
-	{ field: 'name', headerName: 'Name', width: 150 },
-	{ field: 'referralPoints', headerName: 'Referal Pts', width: 100 },
-	{ field: 'bonusPoints', headerName: 'Bonus Pts', width: 100 },
-	{ field: 'totalPoints', headerName: 'Total Pts', width: 100 },
-	{ field: 'caTeamId', headerName: 'Team ID', width: 150 },
-	{ field: 'email', headerName: 'Email ID', width: 250 },
+	{ field: 'name', headerName: 'Name', type: 'string', width: 150 },
+	{
+		field: 'referralPoints',
+		headerName: 'Referal Pts',
+		type: 'number',
+		width: 100,
+	},
+	{
+		field: 'bonusPoints',
+		headerName: 'Bonus Pts',
+		type: 'number',
+		width: 100,
+	},
+	{
+		field: 'totalPoints',
+		headerName: 'Total Pts',
+		type: 'number',
+		width: 100,
+	},
+	{ field: 'caTeamId', headerName: 'Team ID', type: 'number', width: 150 },
+	{ field: 'email', headerName: 'Email ID', type: 'string', width: 250 },
 ];
