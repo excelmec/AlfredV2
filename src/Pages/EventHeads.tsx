276efb1fc41e86ba7613993d284697b2af33e6f8
Jaison Dennis <jaisondennis080@gmail.com>
2023-11-09 15:45:56 +0530

    Did Eventheads list get method

--- src/Pages/EventHeads.tsx
+++ src/Pages/EventHeads.tsx
@@ -1,21 +1,64 @@
+import { useEffect } from "react";
 import { Typography } from "@mui/material";
 import ProtectedRoute from "../Components/Protected/ProtectedRoute";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import {
+  IEventHeadsListItem,
+  useEventHeadsList,
+} from "Hooks/Event/useEventHeadsList";
 
 export default function EventHeadsPage() {
-	return (
-		<ProtectedRoute>
-			<EventHeads />
-		</ProtectedRoute>
-	);
+  return (
+    <ProtectedRoute>
+      <EventHeads />
+    </ProtectedRoute>
+  );
+}
+
+function getRowId(row: IEventHeadsListItem) {
+  return row.id;
 }
 
 export function EventHeads() {
+  const { eventHeadsList, fetchEventHeadsList, error, loading, columns } =
+    useEventHeadsList();
 
-	return (
-		<>
-			<Typography variant='h5' noWrap component='div'>
-				Event Heads List
-			</Typography>
-		</>
-	);
+  useEffect(() => {
+    fetchEventHeadsList();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (error) {
+    return <Typography variant="h5">{error}</Typography>;
+  }
+
+  return (
+    <>
+      <Typography variant="h5" noWrap component="div">
+        Event Heads List
+      </Typography>
+      <br />
+      <DataGrid
+        density="compact"
+        getRowId={getRowId}
+        rows={eventHeadsList}
+        columns={columns}
+        editMode="row"
+        loading={loading}
+        sx={{
+          width: "90%",
+        }}
+        autoPageSize
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+          },
+        }}
+        showCellVerticalBorder
+        showColumnVerticalBorder
+      />
+    </>
+  );
 }
