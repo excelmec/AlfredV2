2e6c6c611bdd895cbb9fa7ea1bcd634920983352
Hemant Antony <88136054+HemantAntony@users.noreply.github.com>
2025-11-22 15:36:39 +0530

    Checking code format check GitHub action

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,13 @@ import TicketDescPage from './Pages/Ticket/TicketDesc';
 import EventScheduleCreate from 'Pages/Events/EventScheduleCreate';
 
 function App() {
+
+  
+
+  console.log("GitHub Action should fail here")
+
+
+  
   return (
     <div className="App">
       <CssBaseline />
